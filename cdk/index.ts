b32590cb3c330786f13a52e813226d13f8c20323
Malik Mahmud <almalikmahmud@gmail.com>
2023-04-02 15:18:57 +0100

    deploy ecs service via CDK

---
+++ cdk/index.ts
@@ -0,0 +1,42 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import path from 'path';
+
+class ApplicationStack extends cdk.Stack {
+  constructor (scope: cdk.App, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+
+    const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'FargateTaskDefinition', {
+      cpu: 512,
+      memoryLimitMiB: 1024
+    });
+
+    taskDefinition.addContainer('WebContainer', {
+      portMappings: [{
+        containerPort: 80
+      }],
+      image: ecs.ContainerImage.fromAsset(path.join(__dirname, '..'))
+    });
+
+    new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Fargateservice', {
+      cluster,
+      desiredCount: 2,
+      publicLoadBalancer: true,
+      taskDefinition
+    });
+  }
+}
+
+const app = new cdk.App();
+new ApplicationStack(app, 'ApplicationStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
