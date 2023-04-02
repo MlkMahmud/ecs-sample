586317b3160e889da32acc48cda0d61c9840111a
Malik Mahmud <almalikmahmud@gmail.com>
2023-04-02 17:42:18 +0100

    access public internet from API

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import Router from 'koa-router';
+import fetch from 'cross-fetch';
 
 const router = new Router();
 
 router.get('/', (ctx) => {
-  ctx.body = JSON.stringify({ message: 'Hello World 🙄' })
+  ctx.body = { message: 'Hello World 🙄' };
 });
 
 router.post('/', (ctx) => {
@@ -14,11 +16,30 @@ router.post('/', (ctx) => {
     }
   });
   ctx.status = 200;
-  ctx.body = JSON.stringify(payload);
+  ctx.body = payload;
 });
 
 router.get('/health', (ctx) => {
   ctx.status = 200;
+});
+
+// Attempt to access public internet
+router.get('/users', async (ctx) => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=5');
+
+  if (response.ok) {
+    const { results: data } = await response.json();
+    ctx.body = {
+      success: true,
+      data
+    };
+    return;
+  }
+  console.error(response.status);
+  ctx.body = {
+    success: false,
+    message: response.statusText
+  };
 });
 
 export default router;
